dbef58b2084d03810386ff594f17b1f940f334ee
Ден Галашевский <galashevskydeny@icloud.com>
2026-01-06 01:24:25 +0100

    Fix

--- Frames/NotificationFrame.lua
+++ Frames/NotificationFrame.lua
@@ -211,8 +211,8 @@ local function GetGroupedNotification(notification)
 
         -- Удаляем все уведомления о смене области или изучении новой области
         for i = #ConsoleMenu.Notifications, 1, -1 do
-            if ConsoleMenu.Notifications[i].event == "ZONE_CHANGED_NEW_AREA" or ConsoleMenu.Notifications[i].event == "ZONE_CHANGED" or ConsoleMenu.Notifications[i].event == "ZONE_CHANGED_INDOORS" or (ConsoleMenu.Notifications[i].event == "UI_INFO_MESSAGE" and ConsoleMenu.Notifications[i].identifier == 408) then
-                if ConsoleMenu.Notifications[i].event == "UI_INFO_MESSAGE" and ConsoleMenu.Notifications[i].identifier == 408 then
+            if ConsoleMenu.Notifications[i].event == "ZONE_CHANGED_NEW_AREA" or ConsoleMenu.Notifications[i].event == "ZONE_CHANGED" or ConsoleMenu.Notifications[i].event == "ZONE_CHANGED_INDOORS" or (ConsoleMenu.Notifications[i].event == "UI_INFO_MESSAGE" and ConsoleMenu.Notifications[i].identifier == 408 and ConsoleMenu.Notifications[i].text == zoneText) then
+                if ConsoleMenu.Notifications[i].event == "UI_INFO_MESSAGE" and ConsoleMenu.Notifications[i].identifier == 408 and ConsoleMenu.Notifications[i].text == zoneText then
                     notification.caption = ConsoleMenu.Notifications[i].caption
                 end
 
